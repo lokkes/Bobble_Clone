49e12cc3101c57e3c0a7731142046da5788dbf59
Manuel Hartmann <manuel.hartmann@stud.htw-dresden.de>
2024-11-28 17:36:52 +0100

    fix resource path

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,7 @@ fn main() -> GameResult {
     let (mut ctx, event_loop) = ContextBuilder::new("bobble_clone", "author_name")
         .window_setup(ggez::conf::WindowSetup::default().title("bobble_clone"))
         .window_mode(WindowMode::default().dimensions(800.0, 480.0))
+        .add_resource_path("./resources")
         .build()?;
 
     let game = game::Game::new(&mut ctx);
